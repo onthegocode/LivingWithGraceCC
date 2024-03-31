751528a3d6f9577e577ec09280441752244420a2
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2024-03-31 13:09:24 -0700

    Add files via upload

---
+++ Post/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (e) {
+	const nav = document.querySelector(".navMenu");
+	const burgerMenu = document.querySelector(".burgerMenuLink");
+	const body = document.querySelector("body");
+	let resized = window.innerWidth;
+	window.addEventListener("resize", () => {
+		console.log(resized);
+		if (resized > 960) {
+			console.log("yes");
+			body.classList.remove("noMove");
+			nav.classList.remove("open");
+			burgerMenu.classList.remove("open");
+		}
+		resized = window.innerWidth;
+	});
+
+	// Function
+	const mobileMenu = function () {
+		nav.addEventListener("click", (e) => {
+			if (e.target.tagName.toLowerCase() === "a" && resized < 960) {
+				body.classList.toggle("noMove");
+				nav.classList.toggle("open");
+				burgerMenu.classList.toggle("open");
+			}
+		});
+		console.log("test");
+		burgerMenu.addEventListener("click", () => {
+			if (resized < 960) {
+				body.classList.toggle("noMove");
+				nav.classList.toggle("open");
+				burgerMenu.classList.toggle("open");
+			}
+		});
+	};
+
+	mobileMenu();
+});
